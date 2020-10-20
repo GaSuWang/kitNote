fc3b7ad776cd5ae0e8929b58d8e6c776e0ab7709
20160125 <ezn1020@kumoh.ac.kr>
2020-10-20 16:12:04 +0900

    checklist 파일 추가 및 기본 코드 추가

---
+++ src/components/Checklist.vue
@@ -0,0 +1,11 @@
+<div id="todo-list-example">
+  <ul>
+    <li
+      is="todo-item"
+      v-for="(todo, index) in todos"
+      v-bind:key="todo.id"
+      v-bind:title="todo.title"
+      v-on:remove="todos.splice(index, 1)"
+    ></li>
+  </ul>
+</div>
