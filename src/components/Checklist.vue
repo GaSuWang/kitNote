9d9bf935fa869e37f6873e2fd50addf05ba4ecdd
20160125 <ezn1020@kumoh.ac.kr>
2020-10-20 21:22:45 +0900

    checklist 컴포넌트 UI 파트 완료 (App.vue에 배치하지 않았음)

--- src/components/Checklist.vue
+++ src/components/Checklist.vue
@@ -1,11 +1,30 @@
-<div id="todo-list-example">
-  <ul>
-    <li
-      is="todo-item"
-      v-for="(todo, index) in todos"
-      v-bind:key="todo.id"
-      v-bind:title="todo.title"
-      v-on:remove="todos.splice(index, 1)"
-    ></li>
-  </ul>
-</div>
+<template>
+  <div id="todo-list">
+    <ul>
+      <li
+        is="todo-item"
+        v-for="(todo, index) in todos"
+        v-bind:key="todo.id"
+        v-bind:title="todo.title"
+        v-on:remove="todos.splice(index, 1)"
+      ></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["todos"],
+  data: function () {
+    return {};
+  },
+  watch: {
+    todos: {
+      handler() {
+        // todo가 변경되었을 경우 핸들러 추가 바람
+      },
+      deep: true,
+    },
+  },
+};
+</script>
