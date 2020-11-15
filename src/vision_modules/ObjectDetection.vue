c84f1db79472ab5cf295bf234a9f0d1a2d9a54c9
20160125 <ezn1020@kumoh.ac.kr>
2020-11-15 11:33:13 +0900

    Object Detection 모듈 추가

---
+++ src/vision_modules/ObjectDetection.vue
@@ -0,0 +1,27 @@
+import * as cocoSSD from '@tensorflow-models/coco-ssd'
+import * as tf from '@tensorflow/tfjs'
+
+<script>
+export default {
+  methods: {
+    async predict(img) {
+      let predicted = [];
+      let rst = await model.detect(img);
+
+      for (var i = 0; i < rst.length; i++) {
+        predicted.push(rst[i].class);
+
+        if (i == 9) break;
+      }
+
+      return predicted;
+    },
+  },
+
+  async mounted() {
+    model = await cocoSSD.load();
+
+    console.log("model loaded");
+  },
+};
+</script>
