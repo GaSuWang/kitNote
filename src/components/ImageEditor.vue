014761f07df05282a3dc0c0b0ee22bfb6ed67212
20160125 <ezn1020@kumoh.ac.kr>
2020-12-07 05:14:50 +0900

    이미지 에디터 UI Design 변경

--- src/components/ImageEditor.vue
+++ src/components/ImageEditor.vue
@@ -1,35 +1,56 @@
 <template>
   <div class="editor-grid">
-    <button @click.prevent="backgroundMasking()">배경제거</button>
-    <div class="filebox">
-      <label for="img_file">배경선택</label>
-      <input
-        type="file"
-        id="img_file"
-        @change="addBackground"
-        accept="image/*"
+    <div class="button-box">
+      <span v-if="flag[0]">
+        <label for="img_file">배경선택</label>
+        <input
+          type="file"
+          id="img_file"
+          @change="addBackground"
+          accept="image/*"
+        />
+      </span>
+      <button
+        class="imageEditorButton"
+        v-if="flag[1]"
+        @click.prevent="backgroundMasking()"
+      >
+        배경제거
+      </button>
+      <button
+        class="imageEditorButton"
+        v-if="flag[2]"
+        @click.prevent="complete()"
+      >
+        완료
+      </button>
+    </div>
+
+    <div>
+      <label>편집 이미지</label>
+      <canvas id="canvas" width="400px" height="400px"
+        >This browser doesn't support canvas</canvas
+      >
+      <img
+        id="image_hidden"
+        :src="imgSrc"
+        width="400px"
+        height="400px"
+        style="display: none"
       />
     </div>
-    <button>배경합성</button>
-    <canvas id="canvas" width="400px" height="400px"
-      >This browser doesn't support canvas</canvas
-    >
-    <img
-      id="image_hidden"
-      :src="imgSrc"
-      width="400px"
-      height="400px"
-      style="display: none"
-    />
-    <canvas id="background" width="400px" height="400px"
-      >This browser doesn't support canvas</canvas
-    >
-    <img
-      id="background_hidden"
-      width="400px"
-      height="400px"
-      style="display: none"
-    />
+    <div>
+      <label>배경 이미지</label>
+      <canvas id="background" width="400px" height="400px"
+        >This browser doesn't support canvas</canvas
+      >
+      <img
+        id="background_hidden"
+        width="400px"
+        height="400px"
+        style="display: none"
+      />
+    </div>
   </div>
 </template>
 
@@ -42,6 +63,7 @@ export default {
     return {
       imgSrc: this.ImageSrc,
       model: null,
+      flag: [true, false, false],
     };
   },
   async mounted() {
@@ -63,6 +85,9 @@ export default {
       const backgroundElement = document.getElementById("background");
 
       await this.model.predict(canvasElement, backgroundElement);
+
+      this.flag[1] = false;
+      this.flag[2] = true;
     },
 
     addBackground(event) {
@@ -74,17 +99,24 @@ export default {
       reader.onload = async (e) => {
         const image = document.getElementById("background_hidden");
         image.src = e.target.result;
-
         const canvas = document.getElementById("background");
         const ctx = canvas.getContext("2d");
 
         ctx.drawImage(image, 0, 0, 400, 400);
+        this.flag[0] = false;
+        this.flag[1] = true;
       };
 
       reader.readAsDataURL(file);
     },
 
-    openImageEditor() {},
+    complete() {
+      const imageData = document
+        .getElementById("canvas")
+        .getContext("2d")
+        .getImageData(0, 0, 400, 400);
+      this.$emit("complete", imageData);
+    },
   },
 
   watch: {},
